919d7d18ccfe3e8a887ae58be366204fc68df036
gunamulya yasa <gunamulya0301@gmail.com>
2025-02-13 15:27:28 +0700

    update by Gugun

--- config.php
+++ config.php
@@ -5,10 +5,8 @@ $dbname = "railway";
 $user = "postgres";
 $password = "OUCHgADVOdfsEdrWFeUZFFjbcbgFfjIA";
 
-// Membuat koneksi
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
-// Cek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . pg_last_error());
 }
