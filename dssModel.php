e6a8d0f82044c7e6dadc627fa800417d2aef4084
Firdaayu22 <fierdaevi@gmail.com>
2025-02-13 12:53:17 +0700

    update model

--- dssModel.php
+++ dssModel.php
@@ -2,57 +2,70 @@
 include 'dbController.php';
 
 // Fungsi normalisasi untuk metode SAW
-function normalisasiSAW($matrix, $tipe_kriteria) {
+function normalisasi_saw($matrix, $tipe_kriteria) {
     $norm_matrix = [];
-    foreach ($matrix as $j => $col) {
-        $max = max(array_column($matrix, $j));
-        $min = min(array_column($matrix, $j));
-        foreach ($col as $i => $val) {
-            $norm_matrix[$i][$j] = ($tipe_kriteria[$j] == 'benefit') ? ($val / $max) : ($min / $val);
+    $num_cols = count($matrix[0]);
+    
+    for ($j = 0; $j < $num_cols; $j++) {
+        $column = array_column($matrix, $j);
+        $max_val = max($column);
+        $min_val = min($column);
+        
+        foreach ($matrix as $i => $row) {
+            if ($tipe_kriteria[$j] == "benefit") {
+                $norm_matrix[$i][$j] = $row[$j] / $max_val;
+            } else {
+                $norm_matrix[$i][$j] = $min_val / $row[$j];
+            }
         }
     }
     return $norm_matrix;
 }
 
 // Fungsi untuk perhitungan SAW
-function hitungSAW($matrix, $bobot, $tipe_kriteria) {
-    $norm_matrix = normalisasiSAW($matrix, $tipe_kriteria);
+function hitung_saw($matrix, $bobot, $tipe_kriteria) {
+    $norm_matrix = normalisasi_saw($matrix, $tipe_kriteria);
     $hasil = [];
+    
     foreach ($norm_matrix as $i => $row) {
-        $hasil[$i] = array_sum(array_map(function($val, $bobot) {
-            return $val * $bobot;
-        }, $row, $bobot));
+        $hasil[$i] = array_sum(array_map(fn($val, $bobot) => $val * $bobot, $row, $bobot));
     }
+    
     return $hasil;
 }
 
 // Fungsi untuk perhitungan WP
-function hitungWP($matrix, $bobot, $tipe_kriteria) {
-    $bobot_ternormalisasi = array_map(function($b) use ($bobot) {
-        return $b / array_sum($bobot);
-    }, $bobot);
+function hitung_wp($matrix, $bobot, $tipe_kriteria) {
+    $num_cols = count($matrix[0]);
+    
+    foreach ($matrix as $i => $row) {
+        for ($j = 0; $j < $num_cols; $j++) {
+            if ($tipe_kriteria[$j] == "cost") {
+                $matrix[$i][$j] = 1 / $row[$j];
+            }
+        }
+    }
     
     $hasil = [];
     foreach ($matrix as $i => $row) {
-        $hasil[$i] = array_product(array_map(function($val, $bobot, $tipe) {
-            return pow(($tipe == 'cost') ? (1 / $val) : $val, $bobot);
-        }, $row, $bobot_ternormalisasi, $tipe_kriteria));
+        $hasil[$i] = array_product(array_map(fn($val, $bobot) => pow($val, $bobot), $row, $bobot));
     }
     
-    $total = array_sum($hasil);
-    foreach ($hasil as $i => $val) {
-        $hasil[$i] = $val / $total;
-    }
     return $hasil;
 }
 
 // Fungsi untuk perhitungan TOPSIS
-function hitungTOPSIS($matrix, $bobot, $tipe_kriteria) {
+function hitung_topsis($matrix, $bobot, $tipe_kriteria) {
+    $num_cols = count($matrix[0]);
+    $num_rows = count($matrix);
     $norm_matrix = [];
-    foreach ($matrix as $j => $col) {
-        $sqrt_sum = sqrt(array_sum(array_map(fn($x) => pow($x, 2), $col)));
-        foreach ($col as $i => $val) {
-            $norm_matrix[$i][$j] = $val / $sqrt_sum;
+    
+    for ($j = 0; $j < $num_cols; $j++) {
+        $sum_squares = array_sum(array_map(fn($row) => pow($row[$j], 2), $matrix));
+        $sqrt_sum = sqrt($sum_squares);
+        
+        foreach ($matrix as $i => $row) {
+            $norm_matrix[$i][$j] = $row[$j] / $sqrt_sum;
         }
     }
     
@@ -63,17 +76,26 @@ function hitungTOPSIS($matrix, $bobot, $tipe_kriteria) {
         }
     }
     
-    $ideal_pos = [];
-    $ideal_neg = [];
-    foreach ($weighted_matrix[0] as $j => $_) {
-        $col = array_column($weighted_matrix, $j);
-        $ideal_pos[$j] = ($tipe_kriteria[$j] == 'benefit') ? max($col) : min($col);
-        $ideal_neg[$j] = ($tipe_kriteria[$j] == 'benefit') ? min($col) : max($col);
+    $ideal_positif = [];
+    $ideal_negatif = [];
+    for ($j = 0; $j < $num_cols; $j++) {
+        $column = array_column($weighted_matrix, $j);
+        $ideal_positif[$j] = ($tipe_kriteria[$j] == "benefit") ? max($column) : min($column);
+        $ideal_negatif[$j] = ($tipe_kriteria[$j] == "benefit") ? min($column) : max($column);
     }
     
-    $dist_pos = array_map(fn($row) => sqrt(array_sum(array_map(fn($x, $y) => pow($x - $y, 2), $row, $ideal_pos))), $weighted_matrix);
-    $dist_neg = array_map(fn($row) => sqrt(array_sum(array_map(fn($x, $y) => pow($x - $y, 2), $row, $ideal_neg))), $weighted_matrix);
+    $jarak_positif = [];
+    $jarak_negatif = [];
+    foreach ($weighted_matrix as $i => $row) {
+        $jarak_positif[$i] = sqrt(array_sum(array_map(fn($val, $ideal) => pow($val - $ideal, 2), $row, $ideal_positif)));
+        $jarak_negatif[$i] = sqrt(array_sum(array_map(fn($val, $ideal) => pow($val - $ideal, 2), $row, $ideal_negatif)));
+    }
     
-    return array_map(fn($d_pos, $d_neg) => $d_neg / ($d_pos + $d_neg), $dist_pos, $dist_neg);
+    $hasil = [];
+    foreach ($jarak_negatif as $i => $d_neg) {
+        $hasil[$i] = $d_neg / ($jarak_positif[$i] + $d_neg);
+    }
+    
+    return $hasil;
 }
 ?>
